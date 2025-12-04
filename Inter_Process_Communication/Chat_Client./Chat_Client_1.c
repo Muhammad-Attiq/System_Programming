168ce6635efcd66e800d31a29389e6361a8b56f5
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-12-04 07:50:09 -0800

    Create Chat_Client_1.c

---
+++ Inter_Process_Communication/Chat_Client./Chat_Client_1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+int main() {
+    mkfifo("fifo1", 0666);
+    mkfifo("fifo2", 0666);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        int fd = open("fifo2", O_RDONLY);
+        char msg[100];
+        while (1) {
+            int n = read(fd, msg, sizeof(msg));
+            if (n > 0) {
+                msg[n] = '\0';
+                if (strcmp(msg, "exit") == 0) break;
+                time_t t = time(NULL);
+                printf("[Client2 %s] %s\n", ctime(&t), msg);
+            }
+        }
+        close(fd);
+    } else {
+
+        int fd = o
+pen("fifo1", O_WRONLY);
+        char msg[100];
+        while (1) {
+            fgets(msg, sizeof(msg), stdin);
+            msg[strcspn(msg, "\n")] = 0;
+
+            write(fd, msg, strlen(msg) + 1);
+
+            if (strcmp(msg, "exit") == 0) break;
+        }
+        close(fd);
+    }
+    return 0;
+}
