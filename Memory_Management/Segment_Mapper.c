c69decbd26fc1c96be29d55c10b6c328094f2cd1
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-12-13 21:24:35 -0800

    Update Segment_Mapper.c

--- Memory_Management/Segment_Mapper.c
+++ Memory_Management/Segment_Mapper.c
@@ -25,40 +25,29 @@ int main() {
         sscanf(line, "%lx-%lx %4s %*s %*s %*s %s", &start, &end, perms, name); 
  
         if (strstr(name, ".text") || strstr(line, "r-xp")) { 
-            if ((unsigned long)my_function >= start && (unsigned long)my_function <= 
-end) 
-                printf("my_function\t%p\tText\tText\t%s\t%lu\n", (void *)my_function, 
-perms, (end - start)/1024); 
+            if ((unsigned long)my_function >= start && (unsigned long)my_function <= end) 
+                printf("my_function\t%p\tText\tText\t%s\t%lu\n", (void *)my_function, perms, (end - start)/1024); 
         } 
  
         if (strstr(name, ".data") || strstr(line, "rw-p")) { 
-            if ((unsigned long)&global_initialized >= start && (unsigned 
-long)&global_initialized <= end) 
+            if ((unsigned long)&global_initialized >= start && (unsigned long)&global_initialized <= end) 
  
  
-                printf("global_initialized\t%p\tData\tData\t%s\t%lu\n", (void 
-*)&global_initialized, perms, (end - start)/1024); 
+                printf("global_initialized\t%p\tData\tData\t%s\t%lu\n", (void *)&global_initialized, perms, (end - start)/1024); 
         } 
  
         if (strstr(name, ".bss") || strstr(line, "rw-p")) { 
-            if ((unsigned long)&global_uninitialized >= start && (unsigned 
-long)&global_uninitialized <= end) 
-                printf("global_uninitialized\t%p\tBSS\tBSS\t%s\t%lu\n", (void 
-*)&global_uninitialized, perms, (end - start)/1024); 
+            if ((unsigned long)&global_uninitialized >= start && (unsigned long)&global_uninitialized <= end) 
+                printf("global_uninitialized\t%p\tBSS\tBSS\t%s\t%lu\n", (void *)&global_uninitialized, perms, (end - start)/1024); 
         } 
  
         if (strstr(name, "[heap]")) { 
             if ((unsigned long)heap_var >= start && (unsigned long)heap_var <= end) 
-                printf("heap_var\t%p\tHeap\tHeap\t%s\t%lu\n", (void *)heap_var, perms, 
-(end - start)/1024); 
+                printf("heap_var\t%p\tHeap\tHeap\t%s\t%lu\n", (void *)heap_var, perms, (end - start)/1024); 
         } 
         if (strstr(name, "[stack]")) { 
-            if ((unsigned long)&local_var >= start && (unsigned  
- 
- 
-long)&local_var <= end) 
-                printf("local_var\t%p\tStack\tStack\t%s\t%lu\n", (void *)&local_var, perms, 
-(end - start)/1024); 
+            if ((unsigned long)&local_var >= start && (unsigned long)&local_var <= end) 
+                printf("local_var\t%p\tStack\tStack\t%s\t%lu\n", (void *)&local_var, perms, (end - start)/1024); 
         } 
     } 
  
